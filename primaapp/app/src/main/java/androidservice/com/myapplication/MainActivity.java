ce23ac55d4646978c9a0ac6a6a0a0e8a1ef33d78
vincenzo85 <vincenzo.difranco@gmail.com>
2016-07-26 02:18:44 +0200

    1.02
    send some intent to service

--- primaapp/app/src/main/java/androidservice/com/myapplication/MainActivity.java
+++ primaapp/app/src/main/java/androidservice/com/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package androidservice.com.myapplication;
 
+import android.app.IntentService;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startService (View v)
+    {
+        Intent intent = new Intent(this, Myservice.class) ;
+        intent.putExtra(Myservice.KEY, Myservice.NUOVIPOST);
+        startService(intent);
+
+    }
+
+    public void stopService (View v){
+
+        Intent intent = new Intent(this, Myservice.class) ;
+        stopService(intent);
+
+
     }
 }
--- primaapp/app/src/main/java/androidservice/com/myapplication/Myservice.java
+++ primaapp/app/src/main/java/androidservice/com/myapplication/Myservice.java
@@ -2,6 +2,7 @@ package androidservice.com.myapplication;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.widget.Toast;
 
 /**
  * Created by Utente on 26/07/2016.
@@ -12,13 +13,65 @@ public class Myservice extends IntentService {
     public static final int POSTTAG=1;
     public static final String KEY="001";
     //con questa chiave posso chiamare il service in funzione della costante
-    
+
     public Myservice(String name) {
         super(name);
     }
 
     @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Toast.makeText(this, "Service started..", Toast.LENGTH_LONG).show();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+
+        Toast.makeText(this, "Service stop", Toast.LENGTH_LONG).show();
+
+
+        super.onDestroy();
+    }
+
+    @Override
     protected void onHandleIntent(Intent intent) {
+
+        final int i = intent.getIntExtra(KEY, -1);
+
+        String risultato="nessun risultato ricevuto";
+
+
+        switch (i)
+        {
+            case NUOVIPOST:
+
+                synchronized (this)
+                {
+                    int count = 0;
+                    while (count<10)
+                    {
+
+                        try {
+                            wait(1500);
+                            count++;
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+
+
+                break;
+            case POSTTAG:
+                break;
+
+
+        }
+
+
+
 
     }
 }
