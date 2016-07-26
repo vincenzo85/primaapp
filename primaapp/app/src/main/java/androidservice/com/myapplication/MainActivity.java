d22401ae5a8ca1bd3ae88859d6527ea6b3fe9fa8
vincenzo85 <vincenzo.difranco@gmail.com>
2016-07-26 14:29:31 +0200

    1.06 bottone salva dati sul database
    ...

--- primaapp/app/src/main/java/androidservice/com/myapplication/MainActivity.java
+++ primaapp/app/src/main/java/androidservice/com/myapplication/MainActivity.java
@@ -2,16 +2,82 @@ package androidservice.com.myapplication;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.ActionMenuItemView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button save,load;
+    EditText name,email;
+
+    //creo un oggetto della classe haanfler class
+    DataHaandler handler;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        save=(Button)findViewById(R.id.save);
+        load=(Button)findViewById(R.id.load);
+        name=(EditText)findViewById(R.id.name);
+        email=(EditText)findViewById(R.id.email);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                //get the value of name ed email frome edittext
+                String getName=name.getText().toString();
+                String getEmail=email.getText().toString();
+                //l'handler ha bisogno del context
+
+                /*
+                public DataHaandler(Context ctx) {
+                this.ctx =ctx;
+                dataBaseHelper = new DataBaseHelper(ctx);}
+                */
+                handler = new DataHaandler(getBaseContext());
+                //apro il database
+                handler.open();
+                // public Cursor retunData () return sqLiteDatabase.query(TABLE_NAME,new String[]{NAME,EMAIL},null,null,null,null,null,null);}
+                //di conseguenza mi serve un cursor
+                /*Cursor c = handler.retunData(); //controllo se il cursor è al primo elemento faccio un loop
+                if(c.moveToFirst())
+                {
+                    do{
+                        getName = c.getString(0);
+                        getEmail=c.getString(1);
+                    }
+                    while(c.moveToNext());
+                 }*/
+                long id = handler.insertData(getName,getEmail);
+                Toast.makeText(getBaseContext(),"Data inserted",Toast.LENGTH_LONG).show();
+                handler.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        });
+
+
     }
 
     public void startService (View v)
@@ -37,5 +103,7 @@ public class MainActivity extends AppCompatActivity {
         startService(intent);
 
     }
+
+
 
 }
